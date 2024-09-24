2dabe1e1b646e9fc2571e7599c93fd821c770b17
woshisck <woshisck@gmail.com>
2024-09-25 03:13:43 +0800

    update

---
+++ Source/DevKit/Public/AnimeDataAsset.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "AnimeDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class DEVKIT_API UAnimeDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
