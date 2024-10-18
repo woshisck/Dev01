7baaefb6b70a105fa13c14696abe45c3e222c37e
woshisck <woshisck@gmail.com>
2024-10-18 12:59:18 +0800

    add base moveSet

---
+++ Source/DevKit/GameModes/YogGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModularGameMode.h"
+#include "YogGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API AYogGameMode : public AModularGameModeBase
+{
+	GENERATED_BODY()
+	
+};
