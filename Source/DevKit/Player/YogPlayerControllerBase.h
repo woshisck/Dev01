90a9beebdedc54b5fafe96f3f2c31876d9c1ff8b
woshisck <woshisck@gmail.com>
2024-10-18 18:21:00 +0800

    update map level definition, game mode

---
+++ Source/DevKit/Player/YogPlayerControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModularPlayerController.h"
+#include "YogPlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API AYogPlayerControllerBase : public AModularPlayerController
+{
+	GENERATED_BODY()
+	
+};
