20b80eb89ea9f224f6acefbc9f83bfae995f0153
woshisck <woshisck@gmail.com>
2024-10-24 09:55:19 +0800

    add

--- Source/DevKit/Animation/YogAnimInstance.cpp
+++ Source/DevKit/Animation/YogAnimInstance.cpp
@@ -2,4 +2,58 @@
 
 
 #include "YogAnimInstance.h"
+#include "../Character/YogBaseCharacter.h"
+#include "../Character/YogCharacterMovementComponent.h"
+#include "AbilitySystemGlobals.h"
 
+#if WITH_EDITOR
+#include "Misc/DataValidation.h"
+#endif
+
+UYogAnimInstance::UYogAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+void UYogAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	check(ASC);
+	GameplayTagPropertyMap.Initialize(this, ASC);
+
+}
+
+EDataValidationResult UYogAnimInstance::IsDataValid(class FDataValidationContext& Context) const
+{
+	Super::IsDataValid(Context);
+	GameplayTagPropertyMap.IsDataValid(this, Context);
+	return ((Context.GetNumErrors() > 0) ? EDataValidationResult::Invalid : EDataValidationResult::Valid);
+}
+
+void UYogAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (AActor* OwningActor = GetOwningActor())
+	{
+		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+		{
+			InitializeWithAbilitySystem(ASC);
+		}
+	}
+}
+
+void UYogAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	const AYogBaseCharacter* Character = Cast<AYogBaseCharacter>(GetOwningActor());
+	if (!Character)
+	{
+		return;
+	}
+	//[Sunchuankai] Update Ground info from movementComponent;
+	//UYogCharacterMovementComponent* CharMoveComp = CastChecked<UYogCharacterMovementComponent>(Character->GetCharacterMovement());
+	//const FLyraCharacterGroundInfo& GroundInfo = CharMoveComp->GetGroundInfo();
+	//GroundDistance = GroundInfo.GroundDistance;
+}
--- Source/DevKit/Animation/YogAnimInstance.h
+++ Source/DevKit/Animation/YogAnimInstance.h
@@ -4,14 +4,43 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayEffectTypes.h"
+
+
 #include "YogAnimInstance.generated.h"
+
+
+class UAbilitySystemComponent;
 
 /**
  * 
  */
-UCLASS(Blueprintable, meta = (BlueprintSpawnableComponent))
+UCLASS(Config = Game)
 class DEVKIT_API UYogAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	UYogAnimInstance(const FObjectInitializer& ObjectInitializer);
+
+	virtual void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
+
+protected:
+
+#if WITH_EDITOR
+	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif // WITH_EDITOR
+
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	// Gameplay tags that can be mapped to blueprint variables. The variables will automatically update as the tags are added or removed.
+	// These should be used instead of manually querying for the gameplay tags.
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
+	float GroundDistance = -1.0f;
+
 };
