6fb1f7286081546ca55d88ee34a8b280d7148a72
woshisck <woshisck@gmail.com>
2025-01-22 22:00:21 +0800

    add

--- Source/DevKit/AbilitySystem/Abilities/YogGameplayAbility.cpp
+++ Source/DevKit/AbilitySystem/Abilities/YogGameplayAbility.cpp
@@ -1,10 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "../../Character/YogBaseCharacter.h"
 #include "YogGameplayAbility.h"
 #include "YogTargetType.h"
-
+#include "../../Character/YogBaseCharacter.h"
 
 
 
@@ -21,6 +20,7 @@ TArray<FActiveGameplayEffectHandle> UYogGameplayAbility::ApplyEffectContainer(FG
 	FYogGameplayEffectContainerSpec Spec = MakeEffectContainerSpec(ContainerTag, EventData, OverrideGameplayLevel);
 	return ApplyEffectContainerSpec(Spec);
 }
+
 
 
 
