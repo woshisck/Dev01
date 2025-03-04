e3dce974e870c817d0e8b68680f24a4376023a2c
woshisck <woshisck@gmail.com>
2025-03-05 01:26:30 +0800

    add

---
+++ Source/DevKit/AbilitySystem/Abilities/YogGameplayAbility_Melee.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "YogGameplayAbility.h"
+#include "YogGameplayAbility_Melee.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API UYogGameplayAbility_Melee : public UYogGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
