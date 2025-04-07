468e7a2307a6fd1e7cf7c372d0f2ecd5228cbbbf
woshisck <woshisck@gmail.com>
2025-04-08 01:32:47 +0800

    ADD

---
+++ Source/DevKit/Camera/YogCameraController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "YogCameraController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API AYogCameraController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
