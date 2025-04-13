83ba5529ad2cf8632ca6ff61f2488e963792d210
woshisck <woshisck@gmail.com>
2025-04-14 01:50:41 +0800

    add

---
+++ Source/DevKit/Character/YogPawnBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModularPawn.h"
+#include "YogPawnBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API AYogPawnBase : public AModularPawn
+{
+	GENERATED_BODY()
+	
+};
--- Source/DevKit/Mob/MobSpawner.cpp
+++ Source/DevKit/Mob/MobSpawner.cpp
@@ -7,7 +7,7 @@
 AMobSpawner::AMobSpawner()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -22,6 +22,11 @@ void AMobSpawner::BeginPlay()
 void AMobSpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+void AMobSpawner::SpawnMob()
+{
 
 }
 
