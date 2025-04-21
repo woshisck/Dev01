9f82a34d172b53f84d846b71fceea39c4cb3c2e5
woshisck <woshisck@gmail.com>
2025-04-21 20:36:33 +0800

    add

---
+++ Source/DevKit/Character/EnemyCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "YogCharacterBase.h"
+#include "EnemyCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API AEnemyCharacterBase : public AYogCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DevKit/Character/PlayerCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "YogCharacterBase.h"
+#include "PlayerCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVKIT_API APlayerCharacterBase : public AYogCharacterBase
+{
+	GENERATED_BODY()
+	
+};
